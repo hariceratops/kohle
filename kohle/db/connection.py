b4f77a132798af6aaf82a6f52b6dd4dac2cd4df5
Hari Prasad Manoharan <hariprasad12994@gmail.com>
2026-03-08 18:19:21 +0100

    Enable foreign key constraints for sqlite

--- kohle/db/connection.py
+++ kohle/db/connection.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 
@@ -8,4 +8,10 @@ DATABASE_URL = "sqlite:///kohle.db"
 engine = create_engine(DATABASE_URL, future=True)
 session_local = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 base = declarative_base()
+
+@event.listens_for(engine, "connect")
+def enable_sqlite_foreign_keys(dbapi_connection, _):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
 
